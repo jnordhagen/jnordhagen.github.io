1f0fcc1fb08a9b4896c34e226d411f74261deb05
Jakob Nordhagen <jakobn@stanford.edu>
2023-10-07 01:34:14 -0700

    added titles to skill icons

--- src/components/skillsCard/SkillsCard.tsx
+++ src/components/skillsCard/SkillsCard.tsx
@@ -1,4 +1,4 @@
-import { Card, Grid, Icon } from "@mui/material";
+import { Card, Grid, Icon, Tooltip } from "@mui/material";
 
 import ReactLogo from "../../assets/react.svg"
 import MongoLogo from "../../assets/mongodb.svg"
@@ -14,16 +14,19 @@ import './SkillsCard.css';
 
 const SkillsCard = () => {
 
-  const logos: string[] = [PythonLogo, CppLogo, TypescriptLogo, SwiftLogo, ReactLogo, MongoLogo, JupyterLogo, PytorchLogo, TensorflowLogo];
+  const logos: string[] = [PythonLogo, CppLogo, TypescriptLogo, SwiftLogo, ReactLogo, MongoLogo, JupyterLogo, PytorchLogo, TensorflowLogo];  
+  const titles: string[] = ["Python", "C++", "TypeScript", "Swift", "React", "MongoDB", "Jupyter Notebooks", "PyTorch", "TensorFlow"];  
 
   return (
     <Card sx={{padding: 5, borderRadius: 5}}>
       <Grid container columnSpacing={5}>
-        {logos.map((logo) => (
-          <Grid item key={logo}>
-            <Icon sx={{height: "inherit", width: "inherit"}} className="icon">
-              <img src={logo} alt="logo"/>
-            </Icon>
+        {logos.map((logo, i) => (
+          <Grid item key={i}>
+            <Tooltip title={titles[i]}>
+              <Icon sx={{height: "inherit", width: "inherit"}} className="icon">
+                <img src={logo} alt="logo"/>
+              </Icon>
+            </Tooltip>
           </Grid>
         ))}
       </Grid>
